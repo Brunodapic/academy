36b115fda5b1231805231adadf7bed935bcc18e1
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2023-03-14 23:41:39 +0100

    patch

--- 2/routes/robot.js
+++ 2/routes/robot.js
@@ -14,24 +14,19 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/new", async function (req, res, next) {
-  //ts->  const robotData: CreateUserDto = req.body
+  //ts->  const robotData: CreateRobotDto = req.body
   const robotData = req.body;
   try {
-    if(onlyLettersAndNumbers(robotData.name)) {
-      /*const createRobotData = {
-        name: robotData.name,
-      };
-      */
-      //const createRobot = await Robot.create({ name:id });
-      const createRobot = await Robot.create({ name:robotData.name });
+    if (onlyLettersAndNumbers(robotData.name)) {
+      const createRobot = await Robot.create({ name: robotData.name });
 
-      res.json({createRobot})
-    }else{
+      res.json({ createRobot });
+    } else {
       res.status(422).send("username can only have letters and numbers");
     }
   } catch (error) {
-    //mozda nije najbolje slati error na frond ali sta se moze
-    res.sendStatus(400).send(error)
+    //mozda nije najbolje slati error na front ali sta se moze
+    res.sendStatus(400).send(error);
   }
 });
 
