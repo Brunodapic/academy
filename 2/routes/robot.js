1e127698748aec2fda4f65e91684682b8ab8cdef
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2023-03-14 23:39:37 +0100

    add robot

--- 2/routes/robot.js
+++ 2/routes/robot.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const Robot = require("../model/Robot");
+const { onlyLettersAndNumbers } = require("../validationFunctions/index");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -12,39 +13,51 @@ router.get("/", function (req, res, next) {
   }
 });
 
-router.post("/:id", async function (req, res, next) {
-  const id = req.params.id;
-  console.log(id);
-  const createRobot = await Robot.create({ name:id });
+router.post("/new", async function (req, res, next) {
+  //ts->  const robotData: CreateUserDto = req.body
+  const robotData = req.body;
+  try {
+    if(onlyLettersAndNumbers(robotData.name)) {
+      /*const createRobotData = {
+        name: robotData.name,
+      };
+      */
+      //const createRobot = await Robot.create({ name:id });
+      const createRobot = await Robot.create({ name:robotData.name });
 
-  res.json({createRobot})
+      res.json({createRobot})
+    }else{
+      res.status(422).send("username can only have letters and numbers");
+    }
+  } catch (error) {
+    //mozda nije najbolje slati error na frond ali sta se moze
+    res.sendStatus(400).send(error)
+  }
 });
 
 router.get("/:id", async function (req, res, next) {
   const id = req.params.id;
   console.log(id);
-  const findeRobot = await Robot.findOne({ _id: id })
+  const findeRobot = await Robot.findOne({ _id: id });
 
-
-  if(!findeRobot){
-    res.sendStatus(404)
-  }else{
-    res.json({findeRobot})
+  if (!findeRobot) {
+    res.sendStatus(404);
+  } else {
+    res.json({ findeRobot });
   }
 });
 
-
+//test route for fetching via ID
 router.get("/id/:id", async function (req, res, next) {
-    const id = req.params.id;
-    console.log(id);
-    const findeRobot = await Robot.findById(id)
-  
-  
-    if(!findeRobot){
-      res.sendStatus(404)
-    }else{
-      res.json({findeRobot})
-    }
-  });
+  const id = req.params.id;
+  console.log(id);
+  const findeRobot = await Robot.findById(id);
+
+  if (!findeRobot) {
+    res.sendStatus(404);
+  } else {
+    res.json({ findeRobot });
+  }
+});
 
 module.exports = router;
