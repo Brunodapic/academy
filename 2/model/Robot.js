ff8e8eb62b5cd15940924fe85817fdadf3a76978
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2023-03-15 22:41:29 +0100

    branch master clean

--- 2/model/Robot.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-
-const Robot = mongoose.model('Robot',{name:String})
-
-
-module.exports = Robot
--- 2/validationFunctions/index.js
+++
@@ -1,8 +0,0 @@
-const usernameRegex = /^[a-zA-Z0-9]+$/;
-
-function onlyLettersAndNumbers(str) {
-  return usernameRegex.test(str);
-}
-
-module.exports = { onlyLettersAndNumbers };
-
